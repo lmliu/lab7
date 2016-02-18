49ce2ddbce694e04f8f1f00fdbe3dfbda5e23288
Lauren <lmliu@ucsd.edu>
2016-02-18 21:11:06 +0000

    analytics lab

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,20 @@ var projects = require('../projects.json');
  * GET home page.
  */
 
+
+function initializePage () {
+$('.likeBtn').click(function(){console.log("eggs");});
+}
+
 exports.view = function(req, res){
+	projects["grid"] = false;
   	res.render('index', projects);
+ 
 };
+
+exports.viewGrid = function(req, res){
+	projects["grid"] = true;
+  	res.render('index', projects);
+ 
+};
+
